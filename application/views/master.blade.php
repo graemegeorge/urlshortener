52f4fae7f814a29a5bf2e9e531a084179eef4a4f
Graeme George <graemelgeorge@gmail.com>
2012-09-18 18:33:27 +0100

    halfway through url shortener

---
+++ application/views/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>URLsHortener</title>
+	<meta name="viewport" content="width=device-width">
+	{{ HTML::style('laravel/css/style.css') }}
+</head>
+<body>
+	
+	<div class="container">
+		@yield('container')
+	</div>
+	
+</body>
+</html>
